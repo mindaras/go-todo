2890a162d07facd34b01fda740c1634437c69a13
mindaugaslazauskas <mindaugas.lazauskas@zyro.com>
2022-07-18 18:44:47 +0300

    project files

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/mindaras/go-todo/database"
+	"github.com/mindaras/go-todo/todo"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+	database.ConnectDB()
+	defer database.DB.Close()
+
+	api := app.Group("/api")
+	todo.Register(api, database.DB)
+
+	log.Fatal(app.Listen(":8000"))
+}
